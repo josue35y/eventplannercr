36bfdbbf500fc8f678b14f0fe1ed6d11a4fad258
ale <alemccr@gmail.com>
2025-02-26 08:18:26 -0600

    Se añade el NuGet de Linq y se modifica la referencia

--- EventPlannerCR-backend/Logica/LogUsuario.cs
+++ EventPlannerCR-backend/Logica/LogUsuario.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using EventPlannerCR_AccesoDatos;
 
 namespace EventPlannerCR_backend.Logica
