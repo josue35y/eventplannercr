0e24e1f9e6e2de1db66ef4dc3d599677ad544ef9
ale <alemccr@gmail.com>
2025-04-11 16:06:38 -0600

    Arreglao

---
+++ EventPlannerCR-backend/Entidades/Response/ResEventosCercanos.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace EventPlannerCR_backend.Entidades
+{
+    public class ResEventosCercanos : ResBase
+    {
+        public List<Evento> Eventos { get; set; }
+    }
+}
